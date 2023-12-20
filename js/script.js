756abe4f40152632fccec4ad72913635352d7868
SebastianBuftea <sebibuftea85@gmail.com>
2023-12-20 15:44:07 +0100

    [ADD] list of mail

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ const { createApp } = Vue;
 createApp({
         data(){
             return{
-
+                arrayMail:[],
+            }
+        },
+        //alla creazione della pagina eseguo un ciclo for al cui interno carichero attraverso la libreria axios un API mail
+        created(){
+            for(let i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response)=> {
+                    this.arrayMail.push(response.data.response)
+                })
             }
         },
         methods:{
